ed6077fa4d5f02b3f857718c43ae36fefb859e27
Ceyhuncan <ceyhunca82@hotmail.com>
2023-10-28 06:38:17 -0400

    props

---
+++ src/components/card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Card = (props) => {
+ 
+    return (
+            <div className="card">
+                <img src={props.image} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{props.cardTitle}</h5>
+                    <p className="card-text">{props.cardText}</p>
+                    <p className="card-text"><small className="text-body-secondary">{props.updatedTime}</small></p>
+                </div>
+        </div>
+    
+    );
+}
+export default Card;
+
--- src/index.js
+++ src/index.js
@@ -1,8 +1,75 @@
+// we will only use index.js and will create a component. We dont need app.js for now. 
+// An Component is a javascript code consist of a class and a function. 
+// We can use componets with jsx codes to display html outputs to user or 
+// and js codes (event handler codes) provide us interactivity(functionality). 
+//Smallest unit is elements in components.
+
+//import: how it works:  from 'react' module and assing to React.They are all in modules folder.
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Card from './components/card';
+
+//first we will create a functional component here. Later we will crete a class component.
+// function App(){
+//     return <h1>My name is Jay</h1>;
+// }
+
+//or we can write a class component;
+
+// class App2 extends React.Component{
+//     render(){
+//         return <h1>My Name is Jay</h1>
+//     }
+// }
+
+
+// ReactDOM.render(<App/>,document.getElementById('root') ); or we could use query selector;
+
+//ReactDOM.render(<App2/>,document.querySelector('#root'));
+
+// COMPONETS
+
+// function App(){
+//     return
+// }
+
+// if we want to rewrite our function (more modern way);
+
+// const App=()=>{
+//     return
+// }
+
+
+const App = () => {
+  return (
+    <div>
+      <div className="card-group">
+
+        <Card
+          cardTitle="Lara"
+          cardText="Card Text 1"
+          updatedTime=" 1 minutes ago updated"
+          image="https://picsum.photos/id/237/200/300"
+        />
+        <Card
+          cardTitle="Nell"
+          cardText="Card Text 2"
+          updatedTime=" 2 minutes ago updated"
+          image="https://picsum.photos/id/102/200/300"
+        />
+        <Card
+          cardTitle="Ceyhuncan"
+          cardText="Card Text 3"
+          updatedTime=" 3 minutes ago updated"
+          image="https://picsum.photos/id/152/200/300" 
+        />
+      </div>
+    </div>
+  );
+}
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +78,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// ReactDOM.render(<App/>, document.getElementById("root"));
